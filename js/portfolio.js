f3f77c1521a2f88f68d8fb93b6aaeda2106344c3
Sankarshan Kundu <skundu@assurance.com>
2021-11-17 11:09:57 -0500

    place holder projects

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,14 +5,13 @@ $("#portfolioModal").bind("show.bs.modal", function (event) {
   var button = event.relatedTarget;
 
   // Extract info from data-bs-* attributes
-  const porject_id = button.getAttribute("data-portfolio");
+  const porject_id = button.getAttribute("data-portfolio-project");
+  const porject_title = button.getAttribute("data-portfolio-project-title");
   const portfolio_page_link = "./portfolio/" + porject_id + "/index.html";
+  // Update the modal's content.
+  $(".modal-title").text(porject_title);
   $.get(portfolio_page_link, function (html) {
     $("#profileModalContent").html(html);
   });
-
-  // Update the modal's content.
-  $(".modal-title").text(getTitleFromId(porject_id));
 });
 
-getTitleFromId = (id) => id.split("_").join(" ");
