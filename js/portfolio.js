db2c757c64d79b26e0cee7e0eafbc9e262fd0b9f
Sankarshan Kundu <skundu@assurance.com>
2021-11-18 12:24:46 -0500

    update

--- js/portfolio.js
+++ js/portfolio.js
@@ -4,10 +4,11 @@ $("#portfolioModal").bind("show.bs.modal", function (event) {
   // Button that triggered the modal
   var button = event.relatedTarget;
 
-  // Extract info from data-bs-* attributes
+  // Extract info from data-portfolio-* attributes
   const porject_id = button.getAttribute("data-portfolio-project");
   const porject_title = button.getAttribute("data-portfolio-project-title");
   const portfolio_page_link = "./portfolio/" + porject_id + "/index.html";
+
   // Update the modal's content.
   $(".modal-title").text(porject_title);
   $.get(portfolio_page_link, function (html) {
@@ -15,3 +16,6 @@ $("#portfolioModal").bind("show.bs.modal", function (event) {
   });
 });
 
+$("#portfolioModal").bind("hidden.bs.modal", function (event) {
+  $("video").trigger("pause");
+});
