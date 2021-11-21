4d45eec2ad4a3aa2ff581e89dcaba0a6226f3c67
Sudipta <sudipta.rhythm@gmail.com>
2021-11-21 03:44:44 -0500

    content
    add and update

--- samples/Aisatu/story_content/5aEcj3SijmC_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.32.20313.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.526%0D%0AMidwife%20refuses%20to%20help%20and%20reports%20you%20to%20the%20police.%20%0D%0A%0D%0A"}')
