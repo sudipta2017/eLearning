7c9854bc30e69568206080c3a48ecb57005fac83
Sudipta <sudipta.rhythm@gmail.com>
2022-01-24 15:04:25 -0500

    PivotTable
    new demo added in Sample

---
+++ samples/PivotTable/story_content/5gQ8p9oAp85_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.54.25674.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:04.467%0D%0ADownload%20the%20sample%20data%20used%20in%20this%20tutorial%20and%20practice%20creating%20your%20own%20PivotTable.%20%0D%0A%0D%0A"}')
