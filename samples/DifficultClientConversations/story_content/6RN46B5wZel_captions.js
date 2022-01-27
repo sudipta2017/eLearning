668898775f1be1bf608704e268c7d0ba8ad5b2bd
Sudipta <sudipta.rhythm@gmail.com>
2022-01-27 01:40:36 -0500

    Sample
    Added Difficult Client Conversations demo in Sample

---
+++ samples/DifficultClientConversations/story_content/6RN46B5wZel_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.60.26947.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:02.899%0D%0AWhich%20one%20of%20the%20following%20question%20is%20more%20preferable%20to%20you?%0D%0A%0D%0A"}')
